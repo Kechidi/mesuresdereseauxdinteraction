45cb80e1edd26154650b9d1fae14714055f50ccd
Thiziri KECHIDI <thiziri.kechidi@mmtt.fr>
2022-12-12 14:34:36 +0100

    téléchargement les données et les lire avec GraphStream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSourceEdge;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.setProperty("org.graphstream.ui", "swing");
+        Graph g = new DefaultGraph("Graphe");
+        FileSourceEdge fs = new FileSourceEdge();
+
+        fs.addSink(g);
+        g.display();
+
+        try {
+            fs.readAll("/home/tkechidi@mmtt.fr/MesuresRI/donnees.txt");
+        } catch (IOException e) {
+            System.out.println(e);
+
+        }
     }
 }
