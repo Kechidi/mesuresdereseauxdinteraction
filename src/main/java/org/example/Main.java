5e55d4af8e812e8d7cd306a4b1f85796c47267a7
Thiziri KECHIDI <thiziri.kechidi@mmtt.fr>
2022-12-12 15:09:42 +0100

    ajout du coefficient de clustering pour un réseau aléatoire de la même taille et du même degré moyen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSourceEdge;
 
 import java.io.IOException;
+import static org.graphstream.algorithm.Toolkit.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +24,11 @@ public class Main {
             System.out.println(e);
 
         }
+
+
+        System.out.println("Le nombre de noeud est:" + g.getNodeCount());
+        System.out.println("Le degré moyen est  :" + averageDegree(g));
+        System.out.println("Le coefficient de clustering :" + averageClusteringCoefficient(g));
+        System.out.println("coefficient de clustering pour un réseau aléatoire de la même taille et du même degré moyen  :" + averageDegree(g) / g.getNodeCount());
     }
 }
