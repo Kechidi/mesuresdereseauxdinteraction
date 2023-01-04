fa63a2c14ceaf6f49207c2b72fca7d87a933ba4c
thiziri kechidi <thiziri.kechidi@etu.univ-lehavre.fr>
2023-01-04 11:50:26 +0100

    update readme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -84,7 +84,63 @@ public class Main {
 
 
 
+        distanceMoyenne(g);
     }
+
+
+
+    // 5- Calcul de la distance moyenne d'un réseau , en utilisant le parcours en largeur pour 1000 sommets choisis alearoirement :
+
+    public static void distanceMoyenne(Graph graph)
+    {
+        List<Node> l = randomNodeSet(graph, 1000);
+        HashMap<Integer, Integer> distancesMap = new HashMap<Integer, Integer>();
+        double distance = 0;
+        int a = 0;
+        for (int i = 0; i < 1000; i++) {
+            BreadthFirstIterator bfi = new BreadthFirstIterator(l.get(i));
+            while (bfi.hasNext()) {
+                Node Noeud = bfi.next();
+                int key = bfi.getDepthOf(Noeud);
+                if (distancesMap.containsKey(key)) {
+                    int t = distancesMap.get(key);
+                    int value = 1 + t;
+                    distancesMap.put(key, value);
+                } else {
+                    distancesMap.put(key, 1);
+                }
+                distance += bfi.getDepthOf(bfi.next());
+                a++;
+            }
+        }
+        double distMoy = distance / (a);
+
+        //Le calcul prend un peu de temps
+        System.out.println("La distance moyenne calculée avec 1000 noeuds au hasard :" + distMoy);
+        System.out.println("La distance moyenne dans un réseau aléatoire avec les mêmes caractéristiques est :" + Math.log(graph.getNodeCount()) / Math.log(averageDegree(graph)));
+
+        //Enregistrer les données dans fichier.dat
+        String filename1 = "DataDistance.dat";
+        try {
+            String filepath = System.getProperty("user.dir") + File.separator + filename1;
+            FileWriter fw = new FileWriter(filepath);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Integer name : distancesMap.keySet()) {
+                bw.write(String.format(Locale.US, "%6d%20.8f%n", name, (double) distancesMap.get(name) / a));
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 }
