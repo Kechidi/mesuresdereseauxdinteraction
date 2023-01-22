e31951f6126b65ff424dc34aa72908c6d5c5cc73
thiziri kechidi <thiziri.kechidi@etu.univ-lehavre.fr>
2023-01-22 15:34:45 +0100

    add response to question 1

--- src/main/java/org/example/PropagationDansDesReseaux.java
+++ src/main/java/org/example/PropagationDansDesReseaux.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import org.graphstream.algorithm.Toolkit;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.file.FileSourceEdge;
 
 import java.io.IOException;
 
-public class PropagationDansDesReseaux {
+public class PropagationDansDesReseaux{
 
     public static void main(String[] args) {
         System.setProperty("org.graphstream.ui", "swing");
@@ -22,5 +23,19 @@ public class PropagationDansDesReseaux {
             System.out.println(e);
 
         }
+        double beta = 1. / 7;
+        double mu = 1./ 14;
+        System.out.println("Le taux de propagation du virus est : " + beta / mu);
+        System.out.println(" le seuil épidémique du réseau est :");
+        double degreMoyen = Toolkit.averageDegree(g);
+        //
+        System.out.println(" le seuil épidémique du réseau est :"+ degreMoyen);
+        System.out.println("Le seuil théorique d'un réseau aléatoire du même degré moyen : " + 1 / (degreMoyen + 1));
+
+
     }
 }
+
+
+
+
